7078d1c3f4a35ab42821d803575bbe819a2ea430
Charles <charlessullivan@brandeis.edu>
2026-03-23 13:28:06 -0700

    Table, queries, script to aggregate player stats across surfaces + seasons

---
+++ aggregate.py
@@ -0,0 +1,35 @@
+from sqlalchemy import text
+
+from db_connection import engine
+
+if __name__ == "__main__":
+    with engine.connect() as conn:
+        # get the players who need their surface stats updated
+        # either 1. matches have been added for this player since their surface stats were last updated
+        # or 2. the player has no entries in surface stats but any complete entry in match_stats
+        # many old players (pre 1991) have no match stats so I want to exclude them
+        players = conn.execute(text("SELECT p.player_id FROM players AS p "
+                            "JOIN player_surface_stats AS ps ON p.player_id = ps.player_id "
+                            "JOIN match_stats AS ms on p.player_id = ms.player_id "
+                            "JOIN raw_matches AS r on ms.match_id = r.match_id "
+                            "WHERE ps.surface = 'ALL' AND ps.season = 0 "
+                            "GROUP BY p.player_id "
+                            "HAVING MAX(r.time_added) > MIN(ps.last_updated) "
+                            "UNION ALL " 
+                            "SELECT p.player_id FROM players AS p "
+                            "WHERE NOT EXISTS ( "
+                                "SELECT 1 FROM player_surface_stats AS ps WHERE ps.player_id = p.player_id "
+                            ") AND EXISTS ("
+                                "SELECT 1 FROM match_stats AS ms WHERE ms.player_id = p.player_id AND ms.complete_stats "
+                            ")")).fetchall()
+        players = [row[0] for row in players]
+        # also need players who do NOT appear in the surface stats table\
+        if len(players) == 0:
+            print("No players need surface stats updated.")
+        else:
+            # update their surface stats
+            with open("player_stats_query.sql") as f:
+                query = text(f.read())
+
+            conn.execute(query, {"player_ids": players})
+            conn.commit()
---
+++ player_stats_query.sql
@@ -0,0 +1,124 @@
+INSERT INTO player_surface_stats (
+  last_updated,
+  player_id,
+  surface,
+  season,
+  matches_played,
+  won,
+  aces,
+  double_faults,
+  service_points,
+  first_serves_in,
+  first_serve_points_won,
+  second_serve_points_won,
+  service_games,
+  break_points_saved,
+  break_points_faced,
+  return_points,
+  first_serve_return_points,
+  first_serve_return_points_won,
+  second_serve_return_points_won,
+  return_games,
+  break_points_converted,
+  break_points_chances,
+  second_serve_points,
+  service_games_won,
+  second_serve_return_points
+)
+WITH stats AS (
+  SELECT
+    NOW() AS last_updated,
+    p.player_id,
+    t.surface,
+    COALESCE(EXTRACT(YEAR FROM m.match_date)::INTEGER, 0) AS season,
+    COUNT(*) AS matches_played,
+    COUNT(*) FILTER (WHERE ms.won) AS won,
+    SUM(ms.aces) AS aces,
+    SUM(ms.double_faults) AS double_faults,
+    SUM(ms.service_points) AS service_points,
+    SUM(ms.first_serves_in) AS first_serves_in,
+    SUM(ms.first_serve_points_won) AS first_serve_points_won,
+    SUM(ms.second_serve_points_won) AS second_serve_points_won,
+    SUM(ms.service_games) AS service_games,
+    SUM(ms.break_points_saved) AS break_points_saved,
+    SUM(ms.break_points_faced) AS break_points_faced,
+    SUM(ms.return_points) AS return_points,
+    SUM(ms.first_serve_return_points) AS first_serve_return_points,
+    SUM(ms.first_serve_return_points_won) AS first_serve_return_points_won,
+    SUM(ms.second_serve_return_points_won) AS second_serve_return_points_won,
+    SUM(ms.return_games) AS return_games,
+    SUM(ms.break_points_converted) AS break_points_converted,
+    SUM(ms.break_points_chances) AS break_points_chances
+  FROM match_stats AS ms
+  JOIN players AS p ON ms.player_id = p.player_id
+  JOIN matches AS m ON ms.match_id = m.match_id
+  JOIN tournaments AS t ON m.tournament_id = t.tournament_id
+  WHERE t.surface IN ('Clay', 'Grass', 'Hard') AND ms.complete_stats = true AND p.player_id = ANY(:player_ids)
+  GROUP BY GROUPING SETS (
+    (p.player_id, t.surface, EXTRACT(YEAR FROM m.match_date)),
+    (p.player_id, t.surface)
+    )
+  
+  UNION ALL
+  
+  SELECT
+    NOW() AS last_updated,
+    p.player_id,
+    'ALL' as surface,
+    COALESCE(EXTRACT(YEAR FROM m.match_date)::INTEGER, 0) AS season,    
+    COUNT(*) AS matches_played,
+    COUNT(*) FILTER (WHERE ms.won) AS won,
+    SUM(ms.aces) AS aces,
+    SUM(ms.double_faults) AS double_faults,
+    SUM(ms.service_points) AS service_points,
+    SUM(ms.first_serves_in) AS first_serves_in,
+    SUM(ms.first_serve_points_won) AS first_serve_points_won,
+    SUM(ms.second_serve_points_won) AS second_serve_points_won,
+    SUM(ms.service_games) AS service_games,
+    SUM(ms.break_points_saved) AS break_points_saved,
+    SUM(ms.break_points_faced) AS break_points_faced,
+    SUM(ms.return_points) AS return_points,
+    SUM(ms.first_serve_return_points) AS first_serve_return_points,
+    SUM(ms.first_serve_return_points_won) AS first_serve_return_points_won,
+    SUM(ms.second_serve_return_points_won) AS second_serve_return_points_won,
+    SUM(ms.return_games) AS return_games,
+    SUM(ms.break_points_converted) AS break_points_converted,
+    SUM(ms.break_points_chances) AS break_points_chances
+  FROM match_stats AS ms
+  JOIN players AS p ON ms.player_id = p.player_id
+  JOIN matches AS m ON ms.match_id = m.match_id
+  WHERE ms.complete_stats = true
+  GROUP BY GROUPING SETS (
+    (p.player_id, EXTRACT(YEAR FROM m.match_date)),
+    (p.player_id)
+    )
+)
+SELECT
+  *,
+  service_points - first_serves_in AS second_serve_points,
+  (service_games - (break_points_faced - break_points_saved)) AS service_games_won,
+  return_points - first_serve_return_points AS second_serve_return_points
+FROM stats
+ON CONFLICT (player_id, surface, season) DO UPDATE SET
+  last_updated = EXCLUDED.last_updated,
+  matches_played = EXCLUDED.matches_played,
+  won = EXCLUDED.won,
+  aces = EXCLUDED.aces,
+  double_faults = EXCLUDED.double_faults,
+  service_points = EXCLUDED.service_points,
+  first_serves_in = EXCLUDED.first_serves_in,
+  first_serve_points_won = EXCLUDED.first_serve_points_won,
+  second_serve_points_won = EXCLUDED.second_serve_points_won,
+  service_games = EXCLUDED.service_games,
+  break_points_saved = EXCLUDED.break_points_saved,
+  break_points_faced = EXCLUDED.break_points_faced,
+  return_points = EXCLUDED.return_points,
+  first_serve_return_points = EXCLUDED.first_serve_return_points,
+  first_serve_return_points_won = EXCLUDED.first_serve_return_points_won,
+  second_serve_return_points_won = EXCLUDED.second_serve_return_points_won,
+  return_games = EXCLUDED.return_games,
+  break_points_converted = EXCLUDED.break_points_converted,
+  break_points_chances = EXCLUDED.break_points_chances,
+  second_serve_points = EXCLUDED.second_serve_points,
+  service_games_won = EXCLUDED.service_games_won,
+  second_serve_return_points = EXCLUDED.second_serve_return_points;
