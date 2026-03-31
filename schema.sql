e32aa10160a0200e9a5cfab1951f8bf80dcaf071
Charles <charlessullivan@brandeis.edu>
2026-03-31 14:37:46 -0700

    Condense schema into single schema.sql file

--- schema.sql
+++ schema.sql
@@ -89,6 +89,7 @@ CREATE TABLE matches (
     tournament_id TEXT REFERENCES tournaments(tournament_id),
     match_date DATE NOT NULL,
     round TEXT NOT NULL,
+    round_int INTEGER,
     winner_id INTEGER REFERENCES players(player_id),
     loser_id INTEGER REFERENCES players(player_id),
     score TEXT
@@ -181,6 +182,53 @@ CREATE TABLE head_to_head (
     matches_played  INTEGER NOT NULL,
     last_updated    TIMESTAMPTZ NOT NULL,
     PRIMARY KEY (player_id, opponent_id, surface)
+);
+
+CREATE TABLE player_form (
+  matches_total INTEGER NOT NULL,
+  won INTEGER not null,
+  player_id INTEGER REFERENCES players(player_id),
+  surface TEXT not null,
+  last_updated timestamptz not null,
+  weighted_form real
+);
+
+CREATE TABLE elo_history (
+    player_id       INTEGER REFERENCES players(player_id),
+    match_id        INTEGER REFERENCES matches(match_id),
+    surface         TEXT NOT NULL,      -- 'Hard', 'Clay', 'Grass', 'ALL'
+    match_date      DATE NOT NULL,
+    elo_before      REAL NOT NULL,
+    elo_after       REAL NOT NULL,
+    k_factor        REAL NOT NULL,      -- store what K was used, useful for auditing
+    opponent_id     INTEGER REFERENCES players(player_id),
+    opponent_elo    REAL NOT NULL,      -- opponent rating at time of match
+    expected        REAL NOT NULL,      -- E(A) — useful for validation later
+    won             BOOLEAN NOT NULL,
+    PRIMARY KEY (player_id, match_id, surface)
+);
+
+CREATE TABLE averaged_surface_elo_history (
+    player_id INTEGER NOT NULL REFERENCES players(player_id),
+    match_id INTEGER NOT NULL REFERENCES matches(match_id),
+    surface TEXT NOT NULL,
+    match_date DATE NOT NULL,
+
+    expected REAL NOT NULL,
+    won BOOLEAN NOT NULL,
+    averaged_surface_elo REAL NOT NULL,
+
+    overall_surface TEXT NOT NULL DEFAULT 'ALL' CHECK (overall_surface = 'ALL'),
+
+    -- surface-specific elo_history row (same player/match/surface)
+    FOREIGN KEY (player_id, match_id, surface)
+        REFERENCES elo_history(player_id, match_id, surface),
+
+    -- overall elo_history row (same player/match, but surface='ALL')
+    FOREIGN KEY (player_id, match_id, overall_surface)
+        REFERENCES elo_history(player_id, match_id, surface),
+
+    PRIMARY KEY (player_id, match_id, surface)
 );
 
 -- used to keep track of which dates filled from API
