5f6100cc56ac28f1236d4167e2cf5f67a66a61dc
Charles <charlessullivan@brandeis.edu>
2026-05-03 12:01:39 -0700

    stg_matches model

--- dbt/models/staging/tournaments.sql
+++
@@ -1,6 +0,0 @@
-select distinct on (tourney_id)
-    tourney_id as tournament_id,
-    tourney_name as name,
-    tourney_level as tournament_level,
-    surface
-from {{ source('raw', 'raw_matches') }}
